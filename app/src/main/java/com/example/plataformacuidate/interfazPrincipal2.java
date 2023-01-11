9ec394a76d7ed95e3f8fbae71fea61a000c66224
Dyjdfg36 <83386679+Dyjdfg36@users.noreply.github.com>
2023-01-11 09:30:32 -0600

    Avance

---
+++ app/src/main/java/com/example/plataformacuidate/interfazPrincipal2.java
@@ -0,0 +1,14 @@
+package com.example.plataformacuidate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class interfazPrincipal2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interfaz_principal2);
+    }
+}
